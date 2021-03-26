a0a403c1e70ebc74e63ebd31e301793e0bae3ba5
timchen7201 <timchen.nba@gmail.com>
2021-03-26 20:27:39 +0800

    Finsih README

--- src/Home/index.js
+++ src/Home/index.js
@@ -4,16 +4,14 @@ import {React} from 'react'
 const Home=()=>{
     return(
         <div className="container-fluid">
-            <div className="container">
-            <div className="row">
-                <h2>Formula SAE-A</h2>
+            <div className="container mt-5">
+            
+                <h2>Formula Student-Taiwan</h2>
                 {/* <img src="../assets/image/2247162.png"/> */}
+            
+                <iframe width="100%" height="550" src="https://www.youtube.com/embed/gZ0H4kPFZrw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-            <div className="row">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/gZ0H4kPFZrw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-            </div>
-        </div>
     )
 }
 
